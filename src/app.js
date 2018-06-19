442b407fe6abc1535dd877ce8df89308d0780512
5habeeb <ahmadshabeeb93@gmail.com>
2018-06-19 20:08:53 +0200

    set fetch data from database action creator and test method for it

--- src/app.js
+++ src/app.js
@@ -4,22 +4,12 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import selectExpenses from './selectors/Expenses'
+import { startSetExpenses } from './actions/expenses';
 import { Provider } from 'react-redux';
 import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 30000, createdAt: 100000}));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 50000, createdAt: 239800000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 1950000, createdAt: 200000}));
-
-const state = store.getState();
-const visibleExpenses = selectExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-//console.log(store.getState().expenses);
 
 const jsx = (
     <Provider store={store}>
@@ -27,4 +17,8 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById("app"));
+ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById("app"));
+});
--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -18,6 +18,8 @@ export default (state = expensesReducerDefaultSate , action) => {
                     return expense;
                 }
             });
+        case 'SET_EXPENSES':
+            return action.expenses;
         default:
            return state;
     }
